7112b3ca2bd71174ab25b77bf37ac2c2c91db236
Daniel Hackiewicz <dhackiewicz@gmail.com>
2026-05-04 17:30:55 +0200

    full config

---
+++ config/system/settings.php
@@ -0,0 +1,156 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$UncwRXhtRTFtNUQ2aXoxMQ$XWRaMzYT07fCTIcUdtIkqIKYeW9EK2oTxSn8EiYnYYs',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'db',
+                'defaultTableOptions' => [
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                ],
+                'driver' => 'mysqli',
+                'host' => 'db',
+                'password' => 'db',
+                'port' => 3306,
+                'user' => 'db',
+            ],
+        ],
+    ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'mask' => [
+            'backend' => 'EXT:site_package/Resources/Private/Mask/Backend/Templates',
+            'backend_layouts_folder' => '',
+            'backendlayout_pids' => '0',
+            'content' => 'EXT:site_package/Resources/Private/Mask/Frontend/Templates',
+            'content_elements_folder' => '',
+            'json' => 'EXT:site_package/Configuration/Mask/mask.json',
+            'layouts' => 'EXT:site_package/Resources/Private/Mask/Frontend/Layouts',
+            'layouts_backend' => 'EXT:site_package/Resources/Private/Mask/Backend/Layouts',
+            'loader_identifier' => 'json',
+            'override_shared_fields' => '0',
+            'partials' => 'EXT:site_package/Resources/Private/Mask/Frontend/Partials',
+            'partials_backend' => 'EXT:site_package/Resources/Private/Mask/Backend/Partials',
+            'preview' => 'EXT:site_package/Resources/Public/Mask/',
+        ],
+        'powermail' => [
+            'disableIpLog' => '1',
+            'disableMarketingInformation' => '0',
+            'disablePluginInformation' => '0',
+            'disablePluginInformationMailPreview' => '0',
+            'enableCaching' => '0',
+            'replaceIrreWithElementBrowser' => '0',
+        ],
+        'redirects' => [
+            'showCheckIntegrityInfoInReports' => '1',
+            'showCheckIntegrityInfoInReportsSeconds' => '86400',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailpit sendmail -t --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '44a656f4c720fb497cad4bea0245a761d9dd904ca824b56ecbe64c6cc4a601ce5f618cf58fc201c95a906bf404176b04',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'extbase.consistentDateTimeHandling' => true,
+            'frontend.cache.autoTagging' => true,
+            'security.system.enforceAllowedFileExtensions' => true,
+        ],
+        'sitename' => 'Grünegg',
+        'systemMaintainers' => [
+            1,
+            2,
+        ],
+    ],
+];
